e954ebaec65da4fc5abfab5c1d145128a808ca86
Leo Cassarani <me@leocassarani.net>
2017-02-09 11:26:01 +0000

    Rewrite eval1 to be :: Term -> Maybe Term

--- 04-arithmetic-expressions/Arith.hs
+++ 04-arithmetic-expressions/Arith.hs
@@ -21,32 +21,39 @@ isVal t = isNumericVal t
 
 {- Evaluation -}
 
-eval1 :: Term -> Term
+eval1 :: Term -> Maybe Term
 
-eval1 (TmIf TmTrue t2 t3) = t2           -- E-IfTrue
-eval1 (TmIf TmFalse t2 t3) = t3          -- E-IfFalse
-eval1 (TmIf t1 t2 t3) = TmIf t1' t2 t3   -- E-If
-  where t1' = eval1 t1
+eval1 (TmIf TmTrue t2 t3) = Just t2      -- E-IfTrue
+eval1 (TmIf TmFalse t2 t3) = Just t3     -- E-IfFalse
+eval1 (TmIf t1 t2 t3)                    -- E-If
+  = case eval1 t1 of
+      Just t1' -> Just (TmIf t1' t2 t3)
+      Nothing  -> Nothing
 
-eval1 (TmSucc t) = TmSucc t'             -- E-Succ
-  where t' = eval1 t
+eval1 (TmSucc t)                         -- E-Succ
+  = case eval1 t of
+      Just t' -> Just (TmSucc t')
+      Nothing -> Nothing
 
-eval1 (TmPred TmZero) = TmZero           -- E-PredZero
+eval1 (TmPred TmZero) = Just TmZero      -- E-PredZero
 eval1 (TmPred (TmSucc nv))
-  | isNumericVal nv = nv                 -- E-PredSucc
-eval1 (TmPred t) = TmPred t'             -- E-Pred
-  where t' = eval1 t
+  | isNumericVal nv = Just nv            -- E-PredSucc
+eval1 (TmPred t)                         -- E-Pred
+  = case eval1 t of
+      Just t' -> Just (TmPred t')
+      Nothing -> Nothing
 
-eval1 (TmIsZero TmZero) = TmTrue         -- E-IszeroZero
+eval1 (TmIsZero TmZero) = Just TmTrue    -- E-IszeroZero
 eval1 (TmIsZero (TmSucc nv))             -- E-IszeroSucc
-  | isNumericVal nv = TmFalse
-eval1 (TmIsZero t) = TmIsZero t'
-  where t' = eval1 t
+  | isNumericVal nv = Just TmFalse
+eval1 (TmIsZero t)
+  = case eval1 t of
+      Just t' -> Just (TmIsZero t')
+      Nothing -> Nothing
 
-eval1 t = t                              -- No rule applies
+eval1 _ = Nothing                        -- No rule applies
 
 eval :: Term -> Term
-eval t
-  | t == t' = t
-  | otherwise = eval t'
-  where t' = eval1 t
+eval t = case eval1 t of
+           Just t' -> eval t'
+           Nothing -> t
