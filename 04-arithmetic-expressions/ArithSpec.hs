2b7763fba17665b3b55f16c3148d53b9b967db17
Leo Cassarani <me@leocassarani.net>
2017-02-12 17:05:28 +0000

    Add QuickCheck implementations of some of the other theorems and lemmas

--- 04-arithmetic-expressions/ArithSpec.hs
+++ 04-arithmetic-expressions/ArithSpec.hs
@@ -1,10 +1,19 @@
 import Arith
 import Control.Monad (liftM, liftM3)
+import Data.List (nub)
 import Data.Maybe (isNothing)
 import Test.QuickCheck
 
-instance Arbitrary Term where
-  arbitrary = genTerm
+main = mapM_ (quickCheckWith stdArgs { maxSuccess = 5000 })
+  [ prop_constsNoGreaterThanSize
+  , prop_everyValueIsNormalForm
+  , prop_everyBoolNormalFormIsValue
+  , prop_terminationOfEvaluation
+  ]
+
+genBool = oneof [return TmTrue,
+                 return TmFalse,
+                 liftM3 TmIf genBool genBool genBool]
 
 genTerm = oneof [return TmTrue,
                  return TmFalse,
@@ -14,7 +23,36 @@ genTerm = oneof [return TmTrue,
                  liftM TmIsZero genTerm,
                  liftM3 TmIf genTerm genTerm genTerm]
 
-prop_everyValueNormalForm t = isVal t ==> isNormalForm t
-  where isNormalForm = isNothing . eval1
+consts :: Term -> [Term]
+consts TmTrue  = [TmTrue]
+consts TmFalse = [TmFalse]
+consts TmZero  = [TmZero]
+consts (TmSucc t1) = consts t1
+consts (TmPred t1) = consts t1
+consts (TmIsZero t1) = consts t1
+consts (TmIf t1 t2 t3) = nub (consts t1 ++ consts t2 ++ consts t3)
 
-main = quickCheck prop_everyValueNormalForm
+size :: Term -> Int
+size TmTrue  = 1
+size TmFalse = 1
+size TmZero  = 1
+size (TmSucc t1) = size t1 + 1
+size (TmPred t1) = size t1 + 1
+size (TmIsZero t1) = size t1 + 1
+size (TmIf t1 t2 t3) = size t1 + size t2 + size t3 + 1
+
+isNormalForm :: Term -> Bool
+isNormalForm = isNothing . eval1
+
+-- Lemma 3.3.3: The number of distinct constants in a term t is no greater
+-- than the size of t (i.e., |Consts(t)| ≤ size(t))
+prop_constsNoGreaterThanSize = forAll genTerm $ \t -> length (consts t) <= size t
+
+-- Theorem 3.5.7: Every value is in normal form
+prop_everyValueIsNormalForm = forAll genTerm $ \t -> isVal t ==> isNormalForm t
+
+-- Theorem 3.5.8: If t is in normal form, then t is a value (for Boolean values)
+prop_everyBoolNormalFormIsValue = forAll genBool $ \t -> isNormalForm t ==> isVal t
+
+-- Theorem 3.5.12: For every term t there is some normal form t' such that t -*> t'
+prop_terminationOfEvaluation = forAll genTerm $ isNormalForm . eval
