ea23fd121fa80fb56a6cf33d963a049e9dc013d8
Leo Cassarani <me@leocassarani.net>
2017-02-12 13:05:33 +0000

    Use fmap on Maybe values instead of pattern matching on Just/Nothing

--- 04-arithmetic-expressions/Arith.hs
+++ 04-arithmetic-expressions/Arith.hs
@@ -23,35 +23,24 @@ isVal t = isNumericVal t
 
 eval1 :: Term -> Maybe Term
 
-eval1 (TmIf TmTrue t2 t3) = Just t2      -- E-IfTrue
-eval1 (TmIf TmFalse t2 t3) = Just t3     -- E-IfFalse
-eval1 (TmIf t1 t2 t3)                    -- E-If
-  = case eval1 t1 of
-      Just t1' -> Just (TmIf t1' t2 t3)
-      Nothing  -> Nothing
+eval1 (TmIf TmTrue t2 t3) = Just t2            -- E-IfTrue
+eval1 (TmIf TmFalse t2 t3) = Just t3           -- E-IfFalse
+eval1 (TmIf t1 t2 t3)                          -- E-If
+  = fmap (\t1' -> TmIf t1' t2 t3) (eval1 t1)
 
-eval1 (TmSucc t)                         -- E-Succ
-  = case eval1 t of
-      Just t' -> Just (TmSucc t')
-      Nothing -> Nothing
+eval1 (TmSucc t) = fmap TmSucc (eval1 t)       -- E-Succ
 
-eval1 (TmPred TmZero) = Just TmZero      -- E-PredZero
+eval1 (TmPred TmZero) = Just TmZero            -- E-PredZero
 eval1 (TmPred (TmSucc nv))
-  | isNumericVal nv = Just nv            -- E-PredSucc
-eval1 (TmPred t)                         -- E-Pred
-  = case eval1 t of
-      Just t' -> Just (TmPred t')
-      Nothing -> Nothing
+  | isNumericVal nv = Just nv                  -- E-PredSucc
+eval1 (TmPred t) = fmap TmPred (eval1 t)       -- E-Pred
 
-eval1 (TmIsZero TmZero) = Just TmTrue    -- E-IszeroZero
-eval1 (TmIsZero (TmSucc nv))             -- E-IszeroSucc
+eval1 (TmIsZero TmZero) = Just TmTrue          -- E-IszeroZero
+eval1 (TmIsZero (TmSucc nv))                   -- E-IszeroSucc
   | isNumericVal nv = Just TmFalse
-eval1 (TmIsZero t)
-  = case eval1 t of
-      Just t' -> Just (TmIsZero t')
-      Nothing -> Nothing
+eval1 (TmIsZero t) = fmap TmIsZero (eval1 t)   -- E-IsZero
 
-eval1 _ = Nothing                        -- No rule applies
+eval1 _ = Nothing                              -- No rule applies
 
 eval :: Term -> Term
 eval t = case eval1 t of
